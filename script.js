0e62840a5eb617fd932c6c8c88054f057a9564de
Shrish Chaturvedi <syrus989@gmail.com>
2018-04-01 23:50:35 -0500

    added

--- script.js
+++ script.js
@@ -44,6 +44,8 @@ $(document).ready(function () {
         // provider.addScope('email');
         return firebase.auth().signInWithPopup(provider).then(function(result) {
             console.log("success");
+
+            window.location = 'https://krisapache.github.io/Giphy-App/';
             // This gives you a Google Access Token. You can use it to access the Google API.
             var token = result.credential.accessToken;
             // The signed-in user info.
